4d0f66a9db4472df996e009fcf65f8764ae88238
KitKat4191 <92064754+KitKat4191@users.noreply.github.com>
2022-11-04 18:02:01 +0100

    Update DFUNC_Bomb.cs
    Now possible to drop bomb manually in ccrp mode by tapping the trigger instead of holding-

--- Scripts/DFUNC/DFUNC_Bomb.cs
+++ Scripts/DFUNC/DFUNC_Bomb.cs
@@ -102,9 +102,10 @@ namespace SaccFlightAndVehicles
         [SerializeField] int MaxiterationStep = 100;
         [SerializeField] int IterationsBeforeCollisionCheck = 1;
         [Tooltip("The distance between where a bomb will land and where a possible target is located, if the ditance is below this value the HUD will tell you how to hit the target, and the script enters CCRP mode (more about that on line 123)")]
-        [SerializeField] float MinDistanceToTarget = 700;
+        [SerializeField] float MinDistanceToTarget = 1000;
         [Tooltip("How fast the red distance diamond goes down the line. A higher number allows the pilot to be more accurate.")]
-        public float multiplier = 0.05f;
+        public float multiplier = 0.005f;
+        public float VerticalBounds = 9f;
         [Tooltip("How close the pilot needs needs to aim to the target before the script drops the bomb. In meters of course.")]
         public float CCRP_Acc_Threshold = 5;
         public LayerMask AGMTargetsLayer = 1 << 26;
@@ -114,7 +115,8 @@ namespace SaccFlightAndVehicles
         Vector3 CCIPLookPos;
         
         //[Header("This makes it look better")]
-        float CCIPverticaloffset = 0f;
+        float CCIPverticaloffset = 8.5f;
+        float CCRPReleaseClamp = 90;
 
         float ClosestDistance;
         float CCRPHeading = 0;
@@ -128,6 +130,8 @@ namespace SaccFlightAndVehicles
         bool CCRPmode = false; //I added "CCRPmode" this mode makes it so when the trigger/spacebar is held the plane automatically drops the bomb whenever the bomb will hit close enough to the target.
         bool CCRPfired;
         bool CCRPheld;
+
+        float CCRPTimer = 0;
 
         float Gravity = Physics.gravity.magnitude * -1;
 
@@ -326,6 +330,7 @@ namespace SaccFlightAndVehicles
                     }
                     else
                     {
+                        CCRPTimer += Time.deltaTime;
                         CCRPheld = true;
                         if (!CCRPfired)
                         {
@@ -340,7 +345,18 @@ namespace SaccFlightAndVehicles
                     }
                     TriggerLastFrame = true;
                 }
-                else { TriggerLastFrame = false; CCRPfired = false; CCRPheld = false; }
+                else
+                {
+                    TriggerLastFrame = false;
+                    CCRPfired = false;
+                    if(CCRPheld && CCRPTimer < 0.75 && CCRPmode && NumBomb > 0 && (AllowFiringWhenGrounded || !(bool)SAVControl.GetProgramVariable("Taxiing")) && ((Time.time - LastBombDropTime) > BombDelay))
+                    {
+                        BombFireFunc();
+                    }
+                    CCRPheld = false;
+                    CCRPTimer = 0;
+                }
+
                 if (!DFUNC_Setup_ERR)
                 {
                     SimulateTrajectory();
@@ -459,13 +475,9 @@ namespace SaccFlightAndVehicles
                 CrosshairRotator.transform.rotation = Quaternion.Euler(new Vector3(angle, CCRPHeading, 0));
 
                 PredictedImpact.rotation = LineRotator.transform.rotation;
-                DistanceRelease = Mathf.Clamp(PredictedImpact.InverseTransformPoint(new Vector3(CurrentCCRPtarget.x, 0, CurrentCCRPtarget.y)).z * -multiplier, -90, 90);
-                TimingRotator.transform.localRotation = Quaternion.Euler(new Vector3(DistanceRelease, 0, 0));
-                if (Mathf.Abs(DistanceRelease) < 60)
-                {
-                    TimingRotator.SetActive(true);
-                }
-                else { TimingRotator.SetActive(false); }
+                DistanceRelease = PredictedImpact.InverseTransformPoint(new Vector3(CurrentCCRPtarget.x, 0, CurrentCCRPtarget.y)).z * multiplier;
+                float DampenedReleaseQue = Mathf.Clamp((float)System.Math.Tanh(DistanceRelease), -CCRPReleaseClamp, CCRPReleaseClamp);
+                TimingRotator.transform.localRotation = Quaternion.Euler(new Vector3(DampenedReleaseQue * -VerticalBounds, 0, 0));
             }
         }
         //CCIP stuff ends here
