1f65e7acb6c5b49633df3a2ee9eda60581b224e2
Sacchan-VRC <67171402+Sacchan-VRC@users.noreply.github.com>
2023-01-31 09:29:41 +0000

    SaccEntitySendEvent: added option to just run a method on the SaccEntity (to go with the recent respawning commit

--- Scripts/Other/SaccEntitySendEvent.cs
+++ Scripts/Other/SaccEntitySendEvent.cs
@@ -10,8 +10,10 @@ namespace SaccFlightAndVehicles
     public class SaccEntitySendEvent : UdonSharpBehaviour
     {
         public SaccEntity EntityControl;
-        [Tooltip("Name of event to send to the SaccEntity")]
-        public string EventName;
+        [Tooltip("Name of entity event to send to the SaccEntity (send to all extensions)")]
+        public string EntityEvent_Name = "SFEXT_O_RespawnButton";
+        [Tooltip("Name of event to send to the SaccEntity (just sent to entity)")]
+        public string Event_Name;
         public bool Global = false;
         public override void Interact()
         {
@@ -26,7 +28,10 @@ namespace SaccFlightAndVehicles
         }
         public void Event()
         {
-            EntityControl.SendEventToExtensions(EventName);
+            if (EntityEvent_Name != string.Empty)
+            { EntityControl.SendEventToExtensions(EntityEvent_Name); }
+            if (Event_Name != string.Empty)
+            { EntityControl.SendCustomEvent(Event_Name); }
         }
     }
 }
