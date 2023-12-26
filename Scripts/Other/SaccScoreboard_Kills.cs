1f7d36ff66c7db8f13ef6dd5d2479170c14bb57b
Sacchan-VRC <67171402+Sacchan-VRC@users.noreply.github.com>
2023-12-26 15:39:35 +0000

    kill scoreboard: use OnDeserialization

--- Scripts/Other/SaccScoreboard_Kills.cs
+++ Scripts/Other/SaccScoreboard_Kills.cs
@@ -13,29 +13,15 @@ namespace SaccFlightAndVehicles
         public Text Scores;
         [System.NonSerializedAttribute, UdonSynced] public string TopKiller = "Nobody";
 
-        [System.NonSerializedAttribute, UdonSynced, FieldChangeCallback(nameof(TopKills))] public ushort _topKills = 0;
-        public ushort TopKills
-        {
-            set
-            {
-                _topKills = value;
-                SendCustomEventDelayedSeconds(nameof(UpdateScores), 1);//TopKiller might not be updated yet if this is done instantly
-            }
-            get => _topKills;
-        }
-        [System.NonSerializedAttribute, UdonSynced, FieldChangeCallback(nameof(DeadVehicles))] public ushort _deadvehicles = 0;
-        public ushort DeadVehicles
-        {
-            set
-            {
-                _deadvehicles = value;
-                SendCustomEventDelayedSeconds(nameof(UpdateScores), 1);
-            }
-            get => _deadvehicles;
-        }
+        [System.NonSerializedAttribute, UdonSynced] public ushort TopKills = 0;
+        [System.NonSerializedAttribute, UdonSynced] public ushort Deadvehicles = 0;
         [System.NonSerializedAttribute] public ushort MyKills = 0;
         [System.NonSerializedAttribute] public ushort MyBestKills = 0;
         private VRCPlayerApi localPlayer;
+        public override void OnDeserialization()
+        {
+            UpdateScores();
+        }
         private void Start()
         {
             localPlayer = Networking.LocalPlayer;
@@ -43,12 +29,13 @@ namespace SaccFlightAndVehicles
         }
         public void PlaneDied()
         {
-            DeadVehicles++;
+            Deadvehicles++;
             RequestSerialization();
+            OnDeserialization();
         }
         public void UpdateScores()
         {
-            Scores.text = string.Concat("Instance Best Killing Spree: ", TopKiller, " : ", TopKills, "\nMy Best Killing Spree: ", MyBestKills, "\nDestroyed Vehicles: ", DeadVehicles);
+            Scores.text = string.Concat("Instance Best Killing Spree: ", TopKiller, " : ", TopKills, "\nMy Best Killing Spree: ", MyBestKills, "\nDestroyed Vehicles: ", Deadvehicles);
         }
         public void UpdateTopKiller()
         {
@@ -56,6 +43,7 @@ namespace SaccFlightAndVehicles
             TopKiller = localPlayer.displayName;
             TopKills = MyKills;
             RequestSerialization();
+            OnDeserialization();
         }
     }
 }
